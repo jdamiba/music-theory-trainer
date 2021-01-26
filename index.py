8f129d49ac271e5ef0bda5348c891171f8736828
Joseph Damiba <joseph.damiba@plot.ly>
2021-01-26 18:40:08 -0500

    expose server for gunicorn

--- index.py
+++ index.py
@@ -8,6 +8,8 @@ external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 app.layout = html.Div(
     [
         html.H4("Music Theory Quiz"),
